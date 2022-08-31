767cb5e606d6a8ea9c45267f557a1b087c98ee10
Johany Lara <joseph.larahi@gmail.com>
2022-08-30 21:04:29 -0500

    Creación del manejador de depencias loader

---
+++ loader.php
@@ -0,0 +1,33 @@
+<?php
+
+/*Importamos las librerias o clases del composer*/
+require_once  "vendor/autoload.php";
+
+/*Importar las entidades*/
+require_once  "model/persistence/entities/usuario.php";
+
+/* Importamos  los controladores*/
+require_once  "controller/controllerUsers.php";
+require_once  "controller/controllerManuals.php";
+
+/* Especificamos el nombre de espacio al que pertenece la clase Manager*/
+use \Illuminate\Database\Capsule\Manager;
+
+/* Creamos una instancia del manager y la configuramos */
+$manager = new Manager();
+	/* Definimos la conexión, es necesario respetar el nombre de los 5 parametros */
+	$manager->addConnection([
+		"driver" => "mysql",
+		"host" => "127.0.0.1",
+		"database" => "inventario",
+		"username" => "root",
+		"password" => ''
+	]);
+	
+	/* La colocamos como global */
+	$manager->setAsGlobal();
+
+	/*La arrancamos o inicializamos */
+	$manager->bootEloquent();
+
+?>
