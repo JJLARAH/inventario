4b7f8aaec1b16bfe0d1fc572a110be934fe5f2c0
Johany Lara <joseph.larahi@gmail.com>
2022-08-31 12:22:55 -0500

    Creación del archivo sources que aloja las importaciones de los scripts para las librerias utilizadas

---
+++ model/scripts/sources.php
@@ -0,0 +1,11 @@
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS  -->
+<script type="text/javascript" src="../../libraries/bootstrap/dist/js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="../../libraries/bootstrap/dist/js/popper.min.js"></script>
+<script type="text/javascript" src="../../libraries/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="../../libraries/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Datatables -->
+<script type="text/javascript" src="../../libraries/DataTables/DataTables-1.11.5/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="../../libraries/DataTables/Responsive-2.3.0/js/dataTables.responsive.min.js"></script>
+<script type="text/javascript" src="../../libraries/DataTables/FixedColumns-4.1.0/js/dataTables.fixedColumns.min.js"></script>
+<script type="text/javascript" src="../../libraries/DataTables/FixedHeader-3.2.3/js/dataTables.fixedHeader.min.js"></script>
