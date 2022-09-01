f0bfc272bb5c7f1dc2669ab5f3ddf5babed88b02
Johany Lara <joseph.larahi@gmail.com>
2022-08-31 19:40:49 -0500

    Agrege readonly para evitar que se edite el formulario de edición a excepción de los campos de estado y comentarios

--- view/administrador/bandeja/editProducto.php
+++ view/administrador/bandeja/editProducto.php
@@ -15,49 +15,49 @@ administrador_session();
 						<div class="col-sm-6">
 							<input type="hidden" name="id_producto" id="id_producto" value="<?= $obj->id_producto ?>">
 							<label>Nombre del producto</label>
-							<input type="text" name="nombre" id="nombre" value="<?= $obj->nombre ?>" class="form-control" required maxlength="30" data-bs-toggle="tooltip" data-bs-placement="top" title="Debe tener máximo 30 caracteres">
+							<input type="text" name="nombre" id="nombre" value="<?= $obj->nombre ?>" class="form-control" maxlength="30" data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo" readonly>
 						</div>
 						<div class="col-sm-6">
 							<label>Descripción</label>
-							<textarea type="text" name="descripcion" id="descripcion" value="<?= $obj->descripcion ?>" class="form-control" required maxlength="100" data-bs-toggle="tooltip" data-bs-placement="top" title="Máximo 100 caracteres"><?= $obj->descripcion ?></textarea>
+							<textarea type="text" name="descripcion" id="descripcion" value="<?= $obj->descripcion ?>" class="form-control" readonly maxlength="100" data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo"><?= $obj->descripcion ?></textarea>
 						</div>
 						<div class="col-sm-3">
 							<label>Categoría</label>
 							<div id="id_categoria">
-								<select name="id_categoria" id="id_categoria" class="form-control form-select" required>
+								<select name="id_categoria" id="id_categoria" class="form-control form-select" data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo">
 									<option value="<?= $obj->id_categoria ?>" selected><?= $obj->categoria ?></option>
-									<option value="1">Electrónica</option>
-									<option value="2">Línea Blanca</option>
-									<option value="3">Deportes</option>
-									<option value="4">Alimentos</option>
-									<option value="5">Jardín</option>
+									<option value="1" disabled>Electrónica</option>
+									<option value="2" disabled>Línea Blanca</option>
+									<option value="3" disabled>Deportes</option>
+									<option value="4" disabled>Alimentos</option>
+									<option value="5" disabled>Jardín</option>
 								</select>
 							</div>
 						</div>
 						<div class="col-sm-3">
 							<label>Sucursal</label>
 							<div id="id_sucursal">
-								<select name="id_sucursal" id="id_sucursal" class="form-control form-select" required>
+								<select name="id_sucursal" id="id_sucursal" class="form-control form-select" readonly data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo">
 									<option value="<?= $obj->id_sucursal ?>" selected><?= $obj->sucursal ?></option>
-									<option value="1">Cuernavaca</option>
-									<option value="2">Yautepec</option>
-									<option value="3">Cuautla</option>
-									<option value="4">Acapulco</option>
+									<option value="1" disabled>Cuernavaca</option>
+									<option value="2" disabled>Yautepec</option>
+									<option value="3" disabled>Cuautla</option>
+									<option value="4" disabled>Acapulco</option>
 								</select>
 							</div>
 						</div>
 						<div class="col-sm-3">
 							<label>Precio</label>
-							<input type="number" name="precio" id="precio" class="form-control" value="<?= $obj->precio ?>" required min="0" max="99999" data-bs-toggle="tooltip" data-bs-placement="top" title="Máximo 5 caracteres">
+							<input type="number" name="precio" id="precio" class="form-control" value="<?= $obj->precio ?>" readonly min="0" max="99999" data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo">
 						</div>
 						<div class="col-sm-3">
 							<label>Fecha de compra</label>
-							<input type="datetime-local" value="<?= $obj->fecha_compra ?>" id="fecha_compra" name="fecha_compra" class="form-control">
+							<input type="datetime-local" value="<?= $obj->fecha_compra ?>" id="fecha_compra" name="fecha_compra" class="form-control" readonly data-bs-toggle="tooltip" data-bs-placement="top" title="No puedes editar este campo">
 						</div>
 						<div class="col-sm-3">
 							<label>Estado</label>
 							<div id="estado">
-								<select name="estado" id="estado" class="form-control form-select" required>
+								<select name="estado" id="estado" class="form-control form-select" required data-bs-toggle="tooltip" data-bs-placement="top" title="Selecciona el estado">
 									<option value="<?= $obj->estado ?>" selected><?= $obj->estado ?></option>
 									<option value="Abierto">Abierto</option>
 									<option value="Cerrado">Cerrado</option>
